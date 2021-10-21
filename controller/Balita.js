fc49512408528d8c3fd1fa4db180e866fdd93e6d
panjiad <panjiawwaludi430@gmail.com>
2021-10-21 11:59:58 +0700

    add console log

--- controller/Balita.js
+++ controller/Balita.js
@@ -3,21 +3,18 @@ const Balita = require("../models/Balita");
 module.exports = {
     index: (req, res) => {
         Balita.findAll().then((rows) => {
-            console.log(rows);
             res.json(rows);
         });
     },
 
     search: (req, res) => {
         Balita.findByPk(req.params.id).then((row) => {
-            console.log(row);
             res.json(row);
         });
     },
 
     store: (req, res) => {
         Balita.create(req.body).then((rows) => {
-            console.log(rows);
             res.json(rows);
         });
     },
@@ -30,7 +27,6 @@ module.exports = {
     },
     destroy: (req, res) => {
         Balita.findByPk(req.params.id).then((row) => {
-            console.log(row);
             row.destroy();
             res.json(row);
         });
